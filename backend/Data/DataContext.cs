a1a7c6a3135581f04b0ab4c4d29f6ba2c8bc3bd4
Fırat Yıldız <fyildiz.cs@gmail.com>
2024-01-10 14:58:50 +0300

    change certs to mac

--- backend/Data/DataContext.cs
+++ backend/Data/DataContext.cs
@@ -3,9 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Data;
 
-public class DataContext : DbContext
+public class DataContext(DbContextOptions options) : DbContext(options)
 {
-    public DataContext(DbContextOptions options) : base(options) { }
-
     public DbSet<AppUser> Users { get; set; }
 }
