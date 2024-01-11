8aea9726fc3a400a48b10317d075a9c188563588
Fırat Yıldız <fyildiz.cs@gmail.com>
2024-01-11 18:37:52 +0300

    add comments

--- backend/Helpers/AutoMapperProfiles.cs
+++ backend/Helpers/AutoMapperProfiles.cs
@@ -10,8 +10,11 @@ public class AutoMapperProfiles : Profile
     {
         CreateMap<AppUser, MemberDTO>()
             .ForMember(
+                // fill this property with options below
                 destination => destination.PhotoUrl,
+                // map to this data from the source itself
                 options => options.MapFrom(source =>
+                    // try to find the main photo, ?? is for cases where photo might be null
                     (source.Photos
                         .FirstOrDefault(photo => photo.IsMain) ?? new Photo { Url = null! }
                     ).Url
